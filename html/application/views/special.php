87001f765e8347c519bd6418ee2a77a003dddbbd
stellunger <1@stellunger.ru>
2014-01-23 18:26:02 +0600

    special

---
+++ html/application/views/special.php
@@ -0,0 +1,79 @@
+	
+	<div id="content">
+		
+		<div class="container">
+		
+			<div class="row">
+				<div id="welcome" class="grid-12">
+					<h1> Акции</h1>
+				</div>
+			</div>
+			
+			
+			<hr class="row-divider" />	
+			
+			
+			<div class="row work-container">
+				
+				<div class="grid-3">
+					<?php echo $sidemenu; ?>			
+					
+				</div>
+				
+				<div class="grid-3">
+					<div class="work-item">
+																		
+						<a class="thumbnail">
+							<img src="<?php echo base_url(); ?>img/alko/ardi-vs.jpg" alt="" />
+						</a> <!-- /img -->
+						
+						<p align="right"><b>Арди VS, французский коньяк, 0,7<br><big>1999 рублей</big></b></p>
+												
+					</div>
+				</div>
+
+				<div class="grid-3">
+					<div class="work-item">
+																		
+						<a class="thumbnail">
+							<img src="<?php echo base_url(); ?>img/alko/glen-morag.jpg" alt="" />
+						</a> <!-- /img -->
+						
+						<p align="right"><b>Глен Моранж, 1-солодовый виски<br>10 лет, 0,7 л<br><big>2990 рублей</big></b></p>
+												
+					</div>
+				</div>
+
+				<div class="grid-3">
+					<div class="work-item">
+																		
+						<a class="thumbnail">
+							<img src="<?php echo base_url(); ?>img/alko/loran.jpg" alt="" />
+						</a> <!-- /img -->
+						
+						<p align="right"><b>Шампанское Лоран Перье (Франция)<br><big>3449 рублей</big></b></p>
+												
+					</div>
+				</div>
+												
+			</div> 
+
+			
+			<div class="row work-container">
+				
+				<div class="grid-3">
+					<h2></h2>					
+					
+				</div>
+				
+				<div class="grid-9">
+					
+					<p align=center><b><big>Подарки с пометкой акция ограниченны по количеству.<br>По наличию уточняйте у оператора.</big></b></p>
+
+				</div>
+								
+			</div> 
+						
+		</div>
+		
+	</div>
